447a90edf0c930ce73b55944c3de3af8022bb115
vkgiri <vkgiri11@gmail.com>
2023-07-23 00:06:27 +0530

    API Error Handling

--- index.js
+++ index.js
@@ -4,9 +4,11 @@ import cors from 'cors';
 
 import connectDB from './config/db.js';
 import userRoutes from './routes/userRoutes.js';
+import { errorHandler, notFound } from './middleware/error.js';
 
 const app = express();
 dotenv.config();
+
 connectDB();
 
 app.use(express.json({ limit: '30mb', extended: true }));
@@ -15,10 +17,9 @@ app.use(cors());
 
 app.use('/api/user', userRoutes);
 
-const PORT = process.env.PORT || 5000;
+app.use(notFound);
+app.use(errorHandler);
 
-app.get('/', (req, res) => {
-	res.send('App is running!!');
-});
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`));
