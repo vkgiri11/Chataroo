ae77482c367878618562bfc1f116da4f784539e2
vkgiri <vkgiri11@gmail.com>
2023-07-23 16:08:57 +0530

    Group Chat controllers

--- controllers/chatController.js
+++ controllers/chatController.js
@@ -65,3 +65,99 @@ export const fetchChats = async (req, res) => {
 		console.log(error);
 	}
 };
+
+export const createGroupChat = async (req, res) => {
+	const { users, group_name } = req.body;
+
+	if (!users || !group_name)
+		return res.status(400).send({ message: 'Please fill in all the fields.' });
+
+	if (users.length < 2)
+		return res.status(400).send({ message: 'More than 2 users required to form a group.' });
+
+	users.push(req.user);
+	try {
+		const createGroupChat = await ChatModel.create({
+			chatName: group_name,
+			users,
+			isGroupChat: true,
+			groupAdmin: req.user,
+		});
+
+		const newGroup = await ChatModel.findOne({ _id: createGroupChat._id })
+			.populate('users', '-password')
+			.populate('groupAdmin', '-password');
+
+		res.status(200).send({ data: newGroup });
+	} catch (error) {
+		res.status(500).json({ message: 'Something went wrong' });
+		console.log(error);
+	}
+};
+
+export const renameGroupChat = async (req, res) => {
+	const { groupName, groupId } = req.body;
+
+	try {
+		const updatedGroup = await ChatModel.findByIdAndUpdate(
+			groupId,
+			{ chatName: groupName },
+			{ new: true }
+		)
+			.populate('users', '-password')
+			.populate('groupAdmin', '-password');
+
+		if (!updatedGroup) return res.status(404).send({ message: 'Group Not Found.' });
+
+		res.status(200).send({ data: updatedGroup });
+	} catch (error) {
+		res.status(500).json({ message: 'Something went wrong' });
+		console.log(error);
+	}
+};
+
+export const addToGroup = async (req, res) => {
+	const { chatId, userId } = req.body;
+
+	// TODO: check if requester is admin or not
+
+	try {
+		const addUser = await ChatModel.findByIdAndUpdate(
+			chatId,
+			{ $push: { user: userId } },
+			{ new: true }
+		)
+			.populate('users', '-password')
+			.populate('groupAdmin', '-password');
+
+		if (!addUser) return res.status(404).send({ message: 'Group Not Found.' });
+
+		res.status(200).send({ data: addUser });
+	} catch (error) {
+		res.status(500).json({ message: 'Something went wrong' });
+		console.log(error);
+	}
+};
+
+export const removeFromGroup = async (req, res) => {
+	const { chatId, userId } = req.body;
+
+	// TODO: check if requester is admin or not
+
+	try {
+		const removeUser = await ChatModel.findByIdAndUpdate(
+			chatId,
+			{ $pull: { user: userId } },
+			{ new: true }
+		)
+			.populate('users', '-password')
+			.populate('groupAdmin', '-password');
+
+		if (!removeUser) return res.status(404).send({ message: 'Group Not Found.' });
+
+		res.status(200).send({ data: removeUser });
+	} catch (error) {
+		res.status(500).json({ message: 'Something went wrong' });
+		console.log(error);
+	}
+};
--- routes/chatRoutes.js
+++ routes/chatRoutes.js
@@ -1,6 +1,13 @@
 import express from 'express';
 
-import { accessChat, fetchChats } from '../controllers/chatController.js';
+import {
+	accessChat,
+	addToGroup,
+	createGroupChat,
+	fetchChats,
+	removeFromGroup,
+	renameGroupChat,
+} from '../controllers/chatController.js';
 import authMiddleware from '../middleware/authMiddleWare.js';
 
 const router = express.Router();
@@ -8,9 +15,9 @@ const router = express.Router();
 router.post('/', authMiddleware, accessChat);
 router.get('/', authMiddleware, fetchChats);
 
-// router.post('/create_group', authMiddleware, createGroupChat)
-// router.put('/rename_group', authMiddleware, renameGroupChat)
-// router.put('/add_to_group', authMiddleware, addToGroup)
-// router.put('/remove_from_group', authMiddleware, removeFromGroup)
+router.post('/create_group', authMiddleware, createGroupChat);
+router.put('/rename_group', authMiddleware, renameGroupChat);
+router.put('/add_to_group', authMiddleware, addToGroup)
+router.put('/remove_from_group', authMiddleware, removeFromGroup)
 
 export default router;
